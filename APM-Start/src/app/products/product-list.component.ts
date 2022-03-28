9741e8ee44bbb26daf61dc7ae0a1c8b7b40e9f07
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-03-28 17:10:02 -0300

    improving change detection

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { catchError, EMPTY, Observable } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
@@ -8,7 +8,8 @@ import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
